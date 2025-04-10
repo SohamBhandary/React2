7edafde2c1a5cfa0f713b10a5003295a04d61968
SohamBhandary <bhandarysoham27@gmail.com>
2025-04-10 13:24:23 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Hero from './Components/Hero'
+import About from './Components/About'
+import NavBar from './Components/NavBar'
+
+
+const App = () => {
+  return (
+    
+     
+    <main className="relative min-h-screen w-screen overflow-x-hidden overflow-y-scroll scrollbar-hide">
+
+     
+      <Hero/>
+       <About/>
+     </main>
+   
+      
+   
+  )
+}
+
+export default App
